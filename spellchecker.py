44ff9bd9320009a41f2abc3de8edde0b3c5338a0
Nick Sweeting <githubpirate@gmail.com>
2014-07-17 13:48:11 -0400

    merged norvig and normal

--- spellchecker.py
+++ spellchecker.py
@@ -1,11 +1,12 @@
 # Nick Sweeting 2014
 # python spellchecker
 
+import re, collections
 from itertools import product, imap
-import re
 
 VERBOSE = True
 vowels = set("aeiouy")
+alphabet = set('abcdefghijklmnopqrstuvwxyz')
 
 def log(*args):
     if VERBOSE:
@@ -14,6 +15,33 @@ def log(*args):
 def words(text):
     """filter body of text for words"""
     return re.findall('[a-z]+', text.lower()) 
+
+def train(text, model=None):
+    """generate word model (dictionary of word:frequency)"""
+    if model is None:
+        model = collections.defaultdict(lambda: 0)
+    for word in text:
+        model[word] += 1
+    return model
+
+def variants(word):
+    """get all possible variants for a word"""
+    splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    deletes    = [a + b[1:] for a, b in splits if b]
+    transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
+    replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
+    inserts    = [a + c + b     for a, b in splits for c in alphabet]
+    log("Deletes: ", len(set(deletes)))
+    log("Transposes: ", len(set(transposes)))
+    log("Replaces: ", len(set(replaces)))
+    log("Inserts: ", len(set(inserts)))
+    s = set(deletes + transposes + replaces + inserts)
+    log("Total Variations Tried: ", len(s))
+    return s
+
+def double_variants(word):
+    """get variants for the variants for a word"""
+    return set(s for w in variants(word) for s in variants(w))
 
 def numberofdupes(string, idx):
     """return the number of times in a row the letter at index idx is duplicated"""
@@ -63,6 +91,15 @@ def flatten(options):
         a.add(''.join(p))
     return a
 
+def norvig_suggestions(word, real_words, short_circuit=True):
+    if short_circuit:
+        return (  {word.lower()} & real_words or
+                        variants(word) & real_words or
+                        double_variants(word) & real_words or
+                        set(["NO SUGGESTION"]))
+    else:
+        return ({word.lower()} | variants(word) | double_variants(word)) & real_words or set(["NO SUGGESTION"])  
+
 def suggestions(word, real_words, short_circuit=True):
     """get best spelling suggestion for word
     return on first match if short_circuit is true, otherwise collect all possible suggestions
@@ -72,7 +109,7 @@ def suggestions(word, real_words, short_circuit=True):
     #  "inSIDE" => "inside"
     if word != word.lower():
         if word.lower() in real_words:
-            return set([word.lower()])
+            return {[word.lower()]}
         word = word.lower()
 
     # Repeated letters:
@@ -108,27 +145,31 @@ def suggestions(word, real_words, short_circuit=True):
 
     return (valid_vowelswaps | valid_reductions | valid_both) or set(["NO SUGGESTION"])
 
-def best_suggestion(inputted_word, suggestions):
+def best_suggestion(inputted_word, suggestions, word_model=None):
     """choose the best suggestion in a list based on lowest hamming distance from original word"""
 
-    scores = {}
-    # give each suggestion a score based on hamming distance
-    for suggestion in suggestions:
-        score = sum(imap(str.__ne__, suggestion, inputted_word[:len(suggestion)]))
-        if score in scores:
-            scores[score] += [suggestion]
-        else:
-            scores[score] = [suggestion]
+    if word_model is None:
+        scores = {}
+        # give each suggestion a score based on hamming distance
+        for suggestion in suggestions:
+            score = sum(imap(str.__ne__, suggestion, inputted_word[:len(suggestion)]))
+            if score in scores:
+                scores[score] += [suggestion]
+            else:
+                scores[score] = [suggestion]
 
-    log("Scores: ", scores)
-
-    # pick the list of suggestions with the lowest hamming distance
-    # return the one that comes first in the alphabet (solves weke-> wake vs wyke)
-    return min(scores[min(scores.keys())])
+        log("Scores: ", scores)
+        # pick the list of suggestions with the lowest hamming distance
+        # return the one that comes first in the alphabet (solves weke-> wake vs wyke)
+        return min(scores[min(scores.keys())])
+    else:
+        log("Scores:", { s:word_model.get(s) for s in suggestions})
+        return max(suggestions, key=word_model.get)
 
 if __name__ == "__main__":
-    word_file = open('/usr/share/dict/words', 'r')
-    real_words = set(words(word_file.read()))
+    word_file = file('/usr/share/dict/words')
+    word_model = train(words(word_file.read()))
+    real_words = set(word_model)
 
     log("Total Word Set: ", len(real_words))
 
@@ -136,9 +177,13 @@ if __name__ == "__main__":
         while True:
             word = str(raw_input(">"))
 
-            possibilities = suggestions(word, real_words, short_circuit=not VERBOSE)
+            possibilities = suggestions(word, real_words, short_circuit=False)
+            n_possibilities = norvig_suggestions(word, real_words, short_circuit=True)
 
-            print(best_suggestion(word, possibilities))
+            print "Hamm Best: " + best_suggestion(word, possibilities)
+            print "Freq Best: " + best_suggestion(word, possibilities, word_model)
+            print "Norvig's Hamm Best: " + best_suggestion(word, n_possibilities)
+            print "Norvig's Freq Best: " + best_suggestion(word, n_possibilities, word_model)
 
     except (EOFError, KeyboardInterrupt):
         exit(0)
